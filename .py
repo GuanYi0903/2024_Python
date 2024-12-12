f88023af4fc64d29e2c344451206ce32e30f08dd
Daniel <daniel.guanyi@gmail.com>
2024-12-12 20:36:17 +0800

    # Without class

--- .py
+++ .py
@@ -1,1 +1,23 @@
-import First
+# Without class
+students = list()
+
+student_1 = {
+    'id': 's10123',
+    'name': 'Tony',
+    'degree': 3
+}
+students.append(student_1)
+
+student_2 = {
+    'id': 's10124',
+    'name': 'Jack',
+    'degree': 3
+}
+students.append(student_2)
+
+student_3 = {
+    'id': 's10125',
+    'name': 'Tim',
+    'degree': 3
+}
+students.append(student_3)
