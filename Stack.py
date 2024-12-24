e0b1ccb68fb92ecf071247ecb71f0659946cd0a1
Daniel <daniel.guanyi@gmail.com>
2024-12-24 21:12:15 +0800

    new

---
+++ Stack.py
@@ -0,0 +1,9 @@
+class Stack ():
+    def _init_(self, args):
+        self.stack_items = [arg for arg in args]
+
+    def peek(self)
+       if len(self.stock_items > 0):
+            return self.stack_items[-1]
+        else:
+            return None
